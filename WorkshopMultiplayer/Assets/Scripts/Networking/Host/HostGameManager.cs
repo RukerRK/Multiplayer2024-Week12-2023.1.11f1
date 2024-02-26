c9b0d4bd1ee86d6745f364fa97c3448741501985
Wolverlone <gamesmews@hotmail.com>
2024-02-26 23:39:49 +0700

    Complete 9-02

--- WorkshopMultiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
+++ WorkshopMultiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -21,7 +21,7 @@ public class HostGameManager : IDisposable
    private string joinCode;
    private string lobbyId;
 
-   private NetworkServer networkServer;
+   public NetworkServer NetworkServer { get; private set; }
 
    private const int MaxConnections = 20;
    private const string GameSceneName = "Game";
@@ -79,7 +79,7 @@ public class HostGameManager : IDisposable
             return;
         }
 
-        networkServer = new NetworkServer(NetworkManager.Singleton);
+        NetworkServer = new NetworkServer(NetworkManager.Singleton);
 
         UserData userData = new UserData
         {
@@ -124,6 +124,6 @@ public class HostGameManager : IDisposable
             lobbyId = string.Empty; 
         }
 
-        networkServer?.Dispose();
+        NetworkServer?.Dispose();
     }
 }
