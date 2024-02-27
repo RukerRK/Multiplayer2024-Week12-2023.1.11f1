aab7743a60bc497eb87119c2fd94bac566921943
Wolverlone <gamesmews@hotmail.com>
2024-02-28 00:24:04 +0700

    Complete 11-01

--- WorkshopMultiplayer/Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ WorkshopMultiplayer/Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -10,6 +11,7 @@ public class Leaderboard : NetworkBehaviour
     [SerializeField] private LeaderboardEntityDisplay leaderboardEntityPrefab;
 
     private NetworkList<LeaderboardEntityState> leaderboardEntities;
+    private List<LeaderboardEntityDisplay> entityDisplays = new List<LeaderboardEntityDisplay>();
 
     private void Awake()
     {
@@ -60,9 +62,34 @@ public class Leaderboard : NetworkBehaviour
         switch (changeEvent.Type)
         {
             case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
-                Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
+                if(!entityDisplays.Any(x=>x.ClientId == changeEvent.Value.ClientId))
+                {
+                   LeaderboardEntityDisplay leaderboardEntity =
+                        Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
+                    leaderboardEntity.Initialise(
+                        changeEvent.Value.ClientId,
+                        changeEvent.Value.PlayerName,
+                        changeEvent.Value.Coins);
+                    entityDisplays.Add(leaderboardEntity);
+                }               
                 break;
             case NetworkListEvent<LeaderboardEntityState>.EventType.Remove:
+                LeaderboardEntityDisplay displayToRemove =
+                    entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+                if (displayToRemove != null)
+                {
+                    displayToRemove.transform.SetParent(null);
+                    Destroy(displayToRemove.gameObject);
+                    entityDisplays.Remove(displayToRemove);
+                }
+                break;
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Value:
+                LeaderboardEntityDisplay displayToUpdate =
+                    entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+                if (displayToUpdate != null)
+                {
+                    displayToUpdate.UpdateCoins(changeEvent.Value.Coins);
+                }
                 break;
         }
     }
--- WorkshopMultiplayer/Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
+++ WorkshopMultiplayer/Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Collections;
 using UnityEngine;
 
 public class LeaderboardEntityDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_Text displayText;
 
-    // Update is called once per frame
-    void Update()
+    private FixedString32Bytes playerName;
+
+    public ulong ClientId { get; private set; }
+    public int Coins { get; private set; }
+
+    public void Initialise(ulong clientId,FixedString32Bytes playerName,int coins)
     {
-        
+        ClientId = clientId;
+        this.playerName = playerName;
+        UpdateCoins(coins);           
+    }
+    public void UpdateCoins(int coins)
+    {
+        Coins = coins;
+        UpdateText();
+    }
+    private void UpdateText()
+    {
+        displayText.text = $"1. {playerName} ({Coins})";
     }
 }
